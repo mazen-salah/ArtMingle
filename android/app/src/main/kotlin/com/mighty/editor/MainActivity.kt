1d9f8a729a6df5ff624709d4d6c892d98b88a081
mazen-salah <mazentamer3056@gmail.com>
2024-06-14 05:09:16 +0300

    updates

--- android/app/src/main/kotlin/com/mighty/editor/MainActivity.kt
+++ android/app/src/main/kotlin/com/mighty/editor/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.mighty.editor
+package com.summationWorks.artmingle
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/utils/AdConfigurationConstants.dart
+++ lib/utils/AdConfigurationConstants.dart
@@ -1,15 +1,17 @@
 const disableAdMob = false;
 
-const String isGoogleAds="admob";
-const String isFacebookAds="facebook";
+const String isGoogleAds = "admob";
+const String isFacebookAds = "facebook";
 
-const isAds=isGoogleAds;
+const isAds = isGoogleAds;
 
-const mAdMobAppId = 'ca-app-pub-1399327544318575~4715757947';
-const mAdMobBannerId = 'ca-app-pub-1399327544318575/8463431267';
-const mAdMobInterstitialId = 'ca-app-pub-1399327544318575/3435940646';
-const mAdMobNativeAdvancedId = 'ca-app-pub-1399327544318575/9584941242';
-const mAdMobRewardedId = 'ca-app-pub-1399327544318575/6958777907';
+const mAdMobAppId = 'ca-app-pub-3433676459697719~4859812849';
+const mAdMobBannerId = 'ca-app-pub-3433676459697719/2592355116';
+const mAdMobInterstitialId = 'ca-app-pub-3433676459697719/7732268639';
+const mAdMobNativeAdvancedId = 'ca-app-pub-3433676459697719/4835375075';
+const mAdMobRewardedId = 'ca-app-pub-3433676459697719/9896130060';
 
-String faceBookBannerPlacementId = "IMG_16_9_APP_INSTALL#4907184199363934_4907186456030375";
-String faceBookInterstitialPlacementId = "IMG_16_9_APP_INSTALL#4907184199363934_4907187922696895";
+String faceBookBannerPlacementId =
+    "IMG_16_9_APP_INSTALL#4907184199363934_4907186456030375";
+String faceBookInterstitialPlacementId =
+    "IMG_16_9_APP_INSTALL#4907184199363934_4907187922696895";
